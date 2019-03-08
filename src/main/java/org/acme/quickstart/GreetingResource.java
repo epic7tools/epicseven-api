49557b173d4e801adbc85e32c028cdcc3d4464b2
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2019-03-08 11:44:56 +1100

    Changed hello endpoint to return a json value

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -1,5 +1,7 @@
 package org.acme.quickstart;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +10,11 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
-    }
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public JsonObject hello() {
+		return Json.createObjectBuilder()
+				.add("hello", "world")
+				.build();
+	}
 }
